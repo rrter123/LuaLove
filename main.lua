bb6783be3f8fa770275a6edbc03148a0bd482923
rrter123 <alicja.gustowska@gmail.com>
2018-05-25 13:38:06 +0200

    Moving avatar added

--- main.lua
+++ main.lua
@@ -2,10 +2,31 @@ local temp_map = require("maps/map_1")
 
 local texture_size_x = 100
 local texture_size_y = 100
+local person_x = 200
+local person_y = 200
+local person_image = love.graphics.newImage("flower.png")
+
 
 local width, height = love.window.getDesktopDimensions()
 print (width, height)
 success = love.window.setMode(width, height)
+love.keyboard.setKeyRepeat(true)
+
+
+function love.update(dt)
+   if love.keyboard.isDown("up") or love.keyboard.isDown("w") then
+    person_y =person_y - 5
+  end
+  if love.keyboard.isDown("down") or love.keyboard.isDown("s") then
+    person_y =person_y + 5
+  end
+  if love.keyboard.isDown("left") or love.keyboard.isDown("a") then
+    person_x =person_x - 5
+  end
+    if love.keyboard.isDown("right") or love.keyboard.isDown("d") then
+    person_x =person_x + 5
+  end
+end
 
 function love.keypressed(key, scancode, isrepeat)
   if key == "escape" then --Pressing Escape closes the window and then schedules the program to close
@@ -23,4 +44,7 @@ function love.draw()
     end
   end
   --texture drawing end
+  --avatar drawing
+  love.graphics.draw(person_image, person_x, person_y)
+  
 end
