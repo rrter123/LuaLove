13dad31575c12ed8f27a8786d6a5265ca0ed3e7a
rrter123 <alicja.gustowska@gmail.com>
2018-05-25 10:27:49 +0200

    Some basic drawings

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Steps to run it on ZBrane on Win
 * In ZeroBrane change the interpreter to Love
 * Have a file named main.lua (?)
 
-I don't know how to format .md files. Yay
+Textures from: http://www.myfreetextures.com
--- main.lua
+++ main.lua
@@ -1,3 +1,12 @@
+local tempmap = require("maps/map_1")
+
+
+
 function love.draw()
-  love.graphics.print("Hello World!", 400, 300)
+  for x, row in ipairs(tempmap) do
+    for y, value in ipairs(row) do
+      local key = "img"..value
+      love.graphics.draw(tempmap[key], (x-1)*100, (y-1)*100)
+    end
+  end
 end
