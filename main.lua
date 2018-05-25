6f833d873eee55dc395cfee33ea2ee6fbfa6bda8
rrter123 <alicja.gustowska@gmail.com>
2018-05-25 10:39:07 +0200

    Name changes and commentary

--- main.lua
+++ main.lua
@@ -1,12 +1,14 @@
-local tempmap = require("maps/map_1")
+local temp_map = require("maps/map_1")
 
+local texture_size_x = 100
+local texture_size_y = 100
 
 
 function love.draw()
-  for x, row in ipairs(tempmap) do
+  for x, row in ipairs(temp_map) do
     for y, value in ipairs(row) do
       local key = "img"..value
-      love.graphics.draw(tempmap[key], (x-1)*100, (y-1)*100)
+      love.graphics.draw(temp_map[key], (x-1)*texture_size_x, (y-1)*texture_size_y)
     end
   end
 end
--- maps/map_1.lua
+++ maps/map_1.lua
@@ -10,6 +10,15 @@ local map =
   { 101, 100, 100, 100, 100, 100, 100, 101 },
   { 101, 101, 101, 101, 101, 101, 101, 101 }
 }
+
+-- The main file reads the array, adds "img" to the number and draws the image from below
+-- The last digit is 0 if the tile is walk-through and 1 if it's a wall, or an obstacle, for an example 100 is walk-through
+-- The middle digit is the number of texture, we can have 10 different textures with the same properties
+-- The first digit informs about the map number, After the first nine it will become a two digit number
+-- !Notice there shouldn't be a map 0, as Lua would interpret the number and the texture names would have to be changed accordingly 
+-- (001 would have to be just 1.jpg)
+
 map["img100"] = love.graphics.newImage("maps/100.jpg")
 map["img101"] = love.graphics.newImage("maps/101.jpg")
+
 return map
