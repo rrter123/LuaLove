e4e0c54a7a323eb5244b9d3180ce02775119c6b9
pala19 <39733330+pala19@users.noreply.github.com>
2018-05-29 23:25:23 +0200

    modified formating from table to string

--- maps.lua
+++ maps.lua
@@ -5,43 +5,14 @@ local map_nr = 3
 
 maps = {}
 
---copied from lua-users.org/wiki/TableUtils
---needed to convert table to string
+--function formating table to string
 
-function table.val_to_str ( v )
-  if "string" == type( v ) then
-    v = string.gsub( v, "\n", "\\n" )
-    if string.match( string.gsub(v,"[^'\"]",""), '^"+$' ) then
-      return "'" .. v .. "'"
-    end
-    return '"' .. string.gsub(v,'"', '\\"' ) .. '"'
-  else
-    return "table" == type( v ) and table.tostring( v ) or
-      tostring( v )
+function tostr(map)
+  str= {}
+  for i=1, #map do
+      str[#str+1] = '{'..table.concat(map[i], ',')..'}'
   end
-end
-
-function table.key_to_str ( k )
-  if "string" == type( k ) and string.match( k, "^[_%a][_%a%d]*$" ) then
-    return k
-  else
-    return "[" .. table.val_to_str( k ) .. "]"
-  end
-end
-
-function table.tostring( tbl )
-  local result, done = {}, {}
-  for k, v in ipairs( tbl ) do
-    table.insert( result, table.val_to_str( v ) )
-    done[ k ] = true
-  end
-  for k, v in pairs( tbl ) do
-    if not done[ k ] then
-      table.insert( result,
-        table.key_to_str( k ) .. "=" .. table.val_to_str( v ) )
-    end
-  end
-  return "{" .. table.concat( result, "," ) .. "}"
+  return '{'..table.concat(str, ',\n')..'}'
 end
 
 --generate new map
@@ -65,7 +36,7 @@ function maps.gen_map(width, length, map_nr) --width, length and number of map w
  --create new .lua file with new map
  
 local new_map = io.open("maps/map_"..map_nr..".lua", "w") 
-new_map:write("map =\n"..table.tostring(map).."\n\n")
+new_map:write("map =\n"..tostr(map).."\n\n")
 new_map:write('map["img'..map_nr..'00"] = love.graphics.newImage("maps/dungeon_textures/200.jpg")\nmap["img'..map_nr..'01"] = love.graphics.newImage("maps/dungeon_textures/201.jpg")\nmap["img'..map_nr..'02"] = love.graphics.newImage("maps/dungeon_textures/202.jpg")\n\nmap["path202"] = "maps/map_'..map_nr..'"\nmap["x102"] = 2\nmap["y102"] = 2\n')
 new_map:write("return map")
 new_map:flush()
