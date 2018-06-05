83891752dc83f4a08569583e9ad0977ff54b9952
pala19 <39733330+pala19@users.noreply.github.com>
2018-06-05 08:37:48 +0200

    changed size of generated map

--- main.lua
+++ main.lua
@@ -27,11 +27,11 @@ end
 
 function load_map(number)
     if number/100 > 100 then
-    maps.gen_map(20,20,(math.floor((number/100)%10)+1),current_map["x"..number],current_map["y"..number])
+    maps.gen_map(30,30,(math.floor((number/100)%10)+1),current_map["x"..number],current_map["y"..number])
     --print("generated new map "..math.abs((number/100)%10))
   end
   if number == 212 then
-    maps.gen_map(20,20,1,current_map["x"..number],current_map["y"..number] )
+    maps.gen_map(30,30,1,current_map["x"..number],current_map["y"..number] )
   end
   path = current_map["path"..number]
   player.player_x = current_map["x"..number]
