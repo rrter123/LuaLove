a8b0c43473010d26af462c9831a39175f6d33449
pala19 <39733330+pala19@users.noreply.github.com>
2018-06-13 10:03:34 +0200

    fixed for battle

--- checks.lua
+++ checks.lua
@@ -14,10 +14,11 @@ function checks.can_go_right(posx, posy, currentmap)
   return currentmap[posy][posx+1]%10==0
 end
 function checks.around(posx, posy, currentmap)
-  if currentmap[posy+1][posx]%10>1 then return currentmap[posy+1][posx] end
-  if currentmap[posy-1][posx]%10>1 then return currentmap[posy-1][posx] end
-  if currentmap[posy][posx+1]%10>1 then return currentmap[posy][posx+1] end
-  if currentmap[posy][posx-1]%10>1 then return currentmap[posy][posx-1] end
+  if currentmap[posy+1][posx]%10>1 then return currentmap[posy+1][posx], posy+1, posx end
+  if currentmap[posy-1][posx]%10>1 then return currentmap[posy-1][posx], posy-1, posx end
+  if currentmap[posy][posx+1]%10>1 then return currentmap[posy][posx+1], posy, posx+1 end
+  if currentmap[posy][posx-1]%10>1 then return currentmap[posy][posx-1], posy, posx-1 end
   return 0 
 end
+
 return checks
